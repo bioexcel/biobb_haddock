8e4199b3807c92a795bf52d7f30c2b6b10dc78be
Pau Andrio <andriopau@gmail.com>
2024-05-21 13:16:51 +0200

    [DOCS] Adding fair software badge and GA

--- biobb_haddock/haddock/__init__.py
+++ biobb_haddock/haddock/__init__.py
@@ -1,2 +1,10 @@
+from . import capri_eval
+from . import clust_fcc
+from . import em_ref
+from . import flex_ref
+from . import rigid_body
+from . import sele_top
+from . import topology
+
 name = "haddock"
 __all__ = ["capri_eval", "clust_fcc", "em_ref", "flex_ref", "rigid_body", "sele_top", "topology"]
