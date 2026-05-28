156a4196255c89b1d93267cb11112e444997762e
gbayarri <genis.bayarri@irbbarcelona.org>
2026-05-28 16:18:43 +0200

    Haddock biobb only via pip

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["docs", "test"]),
     package_data={"biobb_haddock": ["py.typed"]},
     include_package_data=True,
-    install_requires=["biobb_common==5.2.2"],
+    install_requires=["biobb_common==5.2.2", "haddock_biobb==2025.11"],
     python_requires=">=3.10",
     entry_points={
         "console_scripts": [
